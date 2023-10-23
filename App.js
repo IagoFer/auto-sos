817912594e39fee5da845f6ca3bd57f24d6ee8f6
Iago Fernandes Bastos <iago.bastos@dataprev.gov.br>
2023-10-23 16:45:03 -0300

    primeiro-commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import PaginaLogin from './PaginaLogin';
+import PaginaCadastro from './PaginaCadastro';
+import PaginaMenu from './PaginaMenu';
+import PerfilEmpresa from './PerfilEmpresa';
+import PerfilUsuario from './PerfilUsuario';
+import PaginaCadastroEmpresa from './PaginaCadastroEmpresa';
+import { UserProvider } from './UserContext';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Drawer.Navigator initialRouteName="Inicio">
+      <Drawer.Screen name="Inicio" component={PaginaMenu} />
+      <Drawer.Screen name="Seu Perfil" component={PerfilUsuario} />
+      {/* Adicione outras telas de menu aqui, se necessário */}
+    </Drawer.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <UserProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="PaginaLogin">
+          {isLoggedIn ? (
+            <>
+              <Stack.Screen
+                name="Drawer"
+                component={DrawerNavigator}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name="PerfilEmpresa" component={PerfilEmpresa} options={{ headerShown: true }} />
+              {/* Adicione outras telas que devem ter um cabeçalho aqui */}
+            </>
+          ) : (
+            <>
+              <Stack.Screen
+                name="PaginaLogin"
+                component={({ navigation }) => (
+                  <PaginaLogin
+                    navigation={navigation}
+                    setIsLoggedIn={setIsLoggedIn}
+                  />
+                )}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name="PaginaCadastro" component={PaginaCadastro} options={{ headerShown: false }} />
+              <Stack.Screen name="PaginaCadastroEmpresa" component={PaginaCadastroEmpresa} options={{ headerShown: false }} />
+              {/* Adicione outras telas que não devem ter um cabeçalho aqui */}
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserProvider>
+  );
+}
+
+export default App;
---
+++ PaginaMenuEmpresa.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { DrawerContentScrollView } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
+
+const PaginaMenuEmpresa = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      {/* ... Seu conteúdo aqui */}
+      <TouchableOpacity onPress={() => navigation.navigate('PerfilEmpresa')}>
+      <View style={styles.card}>
+                    <Text style={styles.companyName}>Mecânica XYZ</Text>
+                    <Text style={styles.infoText}>Localização: Rua Godofredo, 123</Text>
+                    <Text style={styles.infoText}>Horário de Funcionamento: 08:00 - 18:00</Text>
+                </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5', // Cor de fundo
+        padding: 16,
+    },
+    menuIcon: {
+        marginBottom: 16,
+    },
+    cardContainer: {
+        marginBottom: 16,
+    },
+    card: {
+        backgroundColor: 'white',
+        borderRadius: 8,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        padding: 16,
+    },
+    companyName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 8,
+        color: '#333', // Cor do texto
+    },
+    infoText: {
+        fontSize: 16,
+        marginBottom: 4,
+        color: '#555', // Cor do texto
+    },
+});
+
+export default PaginaMenuEmpresa;
